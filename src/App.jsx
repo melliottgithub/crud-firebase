78a6bf47e6f86c02fd4c37b303f6773fe607a24d
Michael <kllmmc23@gmail.com>
2020-06-14 21:34:13 -0600

    editing all

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { firebase } from "./firebase";
 function App() {
   const [tasks, setTasks] = useState([]);
   const [task, setTask] = useState("");
-  const [modeEdit, setModeEdit] = useState(false);
-  const [id, setId] = useState('');
+  const [edit, setEdit] = useState(false);
+  const [id, setId] = useState("");
 
   useEffect(() => {
     const getData = async () => {
@@ -31,18 +31,20 @@ function App() {
     }
     try {
       const db = firebase.firestore();
+
       const newTask = {
         name: task,
         date: Date.now(),
       };
-      const data = await db.collection("tasks").add(newTask);
-      setTasks([...tasks, { ...newTask, id: data.id }]);
-      setTask("");
-    } catch (error) {
-      alert("otra vez");
-    }
 
-    console.log(task);
+      const data = await db
+        .collection("tasks")
+        .add({ name: task, date: Date.now() });
+
+      setTasks([...tasks, { id: data.id, ...newTask }]);
+
+      setTask("");
+    } catch (error) {}
   };
 
   const deleteHandler = async (id) => {
@@ -56,24 +58,36 @@ function App() {
   };
 
   const actEditHandler = (item) => {
-    setModeEdit(true);
+    setEdit(true);
     setTask(item.name);
     setId(item.id);
   };
 
   const editHandler = async (e) => {
     e.preventDefault();
-    try {
-      
-    } catch (error) {
-      
+    if (!task.trim()) {
+      return;
     }
+    try {
+      const db = firebase.firestore();
+      await db.collection("tasks").doc(id).update({ name: task });
+
+      const arrayEdited = tasks.map((item) =>
+        item.id === id ? { id: item.id, date: item.date, name: task } : item
+      );
+      setTasks(arrayEdited);
+
+      setEdit(false);
+      setId("");
+      setTask("");
+    } catch (error) {}
   };
 
   return (
     <div className="container mt-3">
       <div className="row">
         <div className="col-md-6">
+          <h3>{edit ? "Editing Task List" : "Task list"}</h3>
           <ul className="list-group">
             {tasks.map((item) => (
               <li className="list-group-item" key={item.id}>
@@ -95,9 +109,9 @@ function App() {
           </ul>
         </div>
         <div className="col-md-6">
-          <h3>{modeEdit ? "Edit Task" : "Add Task"}</h3>
+          <h3>{edit ? "Edit Task" : "Add Task"}</h3>
 
-          <form onSubmit={modeEdit ? editHandler : addHandler}>
+          <form onSubmit={edit ? editHandler : addHandler}>
             <input
               type="text"
               placeholder="Enter task"
@@ -108,12 +122,10 @@ function App() {
             <button
               type="submit"
               className={
-                modeEdit
-                  ? "btn btn-warning btn-block"
-                  : "btn btn-dark btn-block"
+                edit ? "btn btn-warning btn-block" : "btn btn-dark btn-block"
               }
             >
-              {modeEdit ? "Edit" : "Add"}
+              {edit ? "Edit" : "Add"}
             </button>
           </form>
         </div>
